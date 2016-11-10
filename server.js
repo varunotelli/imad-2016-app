3d0cbe3cdac51629897ff154f337e4c2765a97ef
varunotelli <varun.kebab@gmail.com>
2016-11-11 04:00:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -20,8 +20,9 @@ app.get('/ui/madi.png', function (req, res) {
 app.get('/ui/bootstrap.css', function (req, res) { res.sendFile(path.join(__dirname, 'ui', 'bootstrap.css')); });
 
 
-app.get('/ui/bootstrap.min.css', function (req, res) { res.sendFile(path.join(__dirname, 'ui', 'bgimg.jpg')); });
+app.get('/ui/bootstrap.min.js', function (req, res) { res.sendFile(path.join(__dirname, 'ui', 'bgimg.jpg')); });
 
+app.get('/ui/bgimg.jpg', function (req, res) { res.sendFile(path.join(__dirname, 'ui', 'bgimg.jpg')); });
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
