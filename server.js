a62adce6d85218ec21dfe949202f5d82195fc9e9
varunotelli <varun.kebab@gmail.com>
2016-11-11 15:02:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -31,6 +31,9 @@ app.get('/ui/bootstrap.css', function (req, res) { res.sendFile(path.join(__dirn
 app.get('/ui/bgimg.jpg', function (req, res) { res.sendFile(path.join(__dirname, 'ui', 'bgimg.jpg')); });
 
 app.get('/ui/profilecss.css', function (req, res) { res.sendFile(path.join(__dirname, 'ui', 'profilecss.css')); });
+
+app.get('/ui/profile.html', function (req, res) { res.sendFile(path.join(__dirname, 'ui', 'profile.html')); });
+
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
   console.log(`IMAD course app listening on port ${port}!`);
