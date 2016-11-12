29fb3e65709763adb047f1d6686f9c26ac2de1b6
varunotelli <varun.kebab@gmail.com>
2016-11-12 23:27:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -57,7 +57,7 @@ app.listen(8080, function () {
 var pool = new Pool(config);
 
 app.get('/test-db', function (req, res) {
-    pool.query('SELECT * FROM test',function(req,res){
+    pool.query('SELECT * FROM test',function(err,result){
         if(err){
            res.status(500).res.send(err.toString());
         }
