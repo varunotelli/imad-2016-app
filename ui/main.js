9f3178c6816f7195fe83e24c915149b76228226e
varunotelli <varun.kebab@gmail.com>
2016-11-13 03:08:32 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,7 @@
 var signup=document.getElementById('signupbtn');
 signup.onclick=function()
 {
-    console.log('in func');
+    //console.log('in func');
     var request=new XMLHttpRequest();
     request.onreadystatechange=function(){
         if(request.readyState===XMLHttpRequest.DONE)
@@ -16,8 +16,11 @@ signup.onclick=function()
 
 
 var username=document.getElementById('usr');
+console.log(username);
 var password=document.getElementById('pwd');
+console.log(password);
 var email=document.getElementById('email');
+console.log(email);
 request.open('POST','http://varunotelli.imad.hasura-app.io/signup',true);
 request.setRequestHeader('Content-Type','application/json');
 request.send(JSON.stringify({username:username,password:password,email:email}));
