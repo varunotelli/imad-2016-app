78963711b7e7cede51648fed312e5d9bfbcbcacb
varunotelli <varun.kebab@gmail.com>
2016-11-17 20:22:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,7 @@
 var signup=document.getElementById('signupbtn');
 signup.onclick=function()
 {
-    //console.log('in func');
+    console.log('in func');
     var request=new XMLHttpRequest();
     request.onreadystatechange=function(){
         if(request.readyState===XMLHttpRequest.DONE)
