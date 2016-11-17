7fd4ca5f8e0794c2b25c6db710ca4e10104b55e7
varunotelli <varun.kebab@gmail.com>
2016-11-17 20:13:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -26,6 +26,15 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+app.get('/ui/favicon-60.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'favicon-60.jpg'));
+});
+
+app.get('/ui/favicon-57.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'favicon-57.jpg'));
+});
+
+
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
