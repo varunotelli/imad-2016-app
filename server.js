0ec5f24197a8b518963f68b5818ef4da94e48665
varunotelli <varun.kebab@gmail.com>
2016-11-17 19:59:41 +0530

    new changes

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pg').Pool;
+var crypto=require('crypto');
+var bodyParser=require('body-parser');
 
+
+var config = {
+  host:'db.imad.hasura-app.io',
+  user: 'varunotelli',
+  password: process.env.DB_PASSWORD,
+  database: 'varunotelli',
+  port:'5432'
+  
+};
 var app = express();
 app.use(morgan('combined'));
+app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
@@ -25,12 +38,165 @@ app.get('/ui/bootstrap.min.css', function (req, res) { res.sendFile(path.join(__
 
 app.get('/ui/bootstrap.css', function (req, res) { res.sendFile(path.join(__dirname, 'ui', 'bootstrap.css')); });
 
-
+app.get('/ui/main.js', function (req, res) { res.sendFile(path.join(__dirname, 'ui', 'main.js')); });
 
 
 app.get('/ui/bgimg.jpg', function (req, res) { res.sendFile(path.join(__dirname, 'ui', 'bgimg.jpg')); });
+
+app.get('/ui/profilecss.css', function (req, res) { res.sendFile(path.join(__dirname, 'ui', 'profilecss.css')); });
+
+app.get('/ui/profile.html', function (req, res) { res.sendFile(path.join(__dirname, 'ui', 'profile.html')); });
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
   console.log(`IMAD course app listening on port ${port}!`);
 });
+
+
+
+
+
+
+
+
+var pool = new Pool(config);
+app.get('/test-db',function(req,res){
+//make a select response
+////return a response with results
+pool.query("SELECT * FROM test",function(err,result){
+    if(err){
+        res.status(500).send(err.toString());
+    }else{
+        res.send(JSON.stringify(result));
+    }
+    
+});
+});
+
+function hash(input,salt)
+{
+    var hashed=crypto.pbkdf2Sync('secret', 'salt', 100000, 512, 'sha512');
+    return["pbkdf2","100000",salt,hashed.toString('hex')].join('$');
+}
+
+
+app.get('/user/:input',function(req,res){
+    var hashedString=hash(req.params.input,"random string");
+    res.send(hashedString);
+});
+
+
+
+
+app.get('/articles/:articleName',function(req,res){
+    
+    var articleName=req.params.articleName;
+    pool.query("SELECT * FROM article where title=$1",[req.params.articleName],function(err,result){
+    if(err){
+        res.status(500).send(err.toString());
+    }else{
+        if(result.rows.length===0)
+        {
+            res.status(404).send('Article not found');
+        }
+        else
+        {
+            var articleDate=result.rows[0];
+            res.send(createTemplate(articleDate));
+        }
+    }
+    
+});
+});
+    
+    
+app.get('/user',function(req,res){
+    
+    pool.query("SELECT * from user",function(err,result){
+        if(err)
+        res.status(500).send(err.toString());
+        else
+        res.send(JSON.stringify(result));
+        
+    });
+    
+});
+
+
+app.post('/signup',function(req,res){
+    
+    var username=req.body.username;
+    var password=req.body.password;
+    var email=req.body.email;
+    var salt=crypto.randomBytes(128).toString('hex');
+    var dbString=hash(password,salt);
+    
+    pool.query('INSERT INTO "user" (username,password,email,type) VALUES($1,$2,$3,$4) ',[username,dbString,email,'A'],function(err,result){
+    if(err){
+        res.status(500).send(err.toString());
+    }else{
+        res.send('Success');
+    }
+    
+});
+});
+
+function createTemplate(data)
+{
+    var title=data.title;
+    var content=data.content;
+    var posted=data.posted;
+    var htmlTemplate=
+    
+        `
+        <html>        
+        <h1>
+        ${title}
+        </h1>
+        <p>${content}</p>
+        <br>
+        <div>Posted on: ${posted.toDateString()}</div>
+        `
+        
+        ;
+     return htmlTemplate;   
+    
+}
+
+var counter=0;
+app.get('/counter',function(req,res){
+counter=counter+1; 
+res.send(counter.toString());
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,62 @@
-console.log('Loaded!');
+var signup=document.getElementById('signupbtn');
+signup.onclick=function()
+{
+    //console.log('in func');
+    var request=new XMLHttpRequest();
+    request.onreadystatechange=function(){
+        if(request.readyState===XMLHttpRequest.DONE)
+        {
+            if(request.status===200)
+            window.location.href='http://varunotelli.imad.hasura-app.io/ui/profile.html'
+            else
+            alert('problem');
+        }
+    };
+
+
+
+var username=document.getElementById('usr').value;
+console.log(username);
+var password=document.getElementById('pwd').value;
+console.log(password);
+var email=document.getElementById('email').value;
+console.log(email);
+request.open('POST','http://varunotelli.imad.hasura-app.io/signup',true);
+request.setRequestHeader('Content-Type','application/json');
+request.send(JSON.stringify({username:username,password:password,email:email}));
+
+};
+
+
+
+
+
+
+var submit=document.getElementById('submit');
+signup.onclick=function()
+{
+    //console.log('in func');
+    var request=new XMLHttpRequest();
+    request.onreadystatechange=function(){
+        if(request.readyState===XMLHttpRequest.DONE)
+        {
+            if(request.status===200)
+            window.location.href='http://varunotelli.imad.hasura-app.io/ui/profile.html'
+            else
+            alert('problem');
+        }
+    };
+
+
+
+var title.getElementById('titletxt').value;
+console.log(title);
+var content=document.getElementById('content').value;
+console.log(content);
+//var email=document.getElementById('email').value;
+//console.log(email);
+request.open('POST','http://varunotelli.imad.hasura-app.io/create-blog',true);
+request.setRequestHeader('Content-Type','application/json');
+request.send(JSON.stringify({title:title,content:content}));
+
+};
