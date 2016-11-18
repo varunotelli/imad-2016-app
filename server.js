f502271f0a412d03e05f1cf6ce5353cfe45495e7
varunotelli <varun.kebab@gmail.com>
2016-11-18 22:08:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -193,6 +193,20 @@ app.post('/signin', function (req, res) {
    });
 });
 
+app.get('/check-login', function (req, res) {
+   if (req.session && req.session.auth && req.session.auth.userId) {
+       // Load the user object
+       pool.query('SELECT * FROM "user" WHERE id = $1', [req.session.auth.userId], function (err, result) {
+           if (err) {
+              res.status(500).send(err.toString());
+           } else {
+              res.send(result.rows[0].username);    
+           }
+       });
+   } else {
+       res.status(400).send('You are not logged in');
+   }
+});
 
 app.get('/logout',function(req,res)
 {
