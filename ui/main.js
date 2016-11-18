98e683e28531cc8248f560169a254a37eb12d4b9
varunotelli <varun.kebab@gmail.com>
2016-11-18 23:24:59 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -126,6 +126,7 @@ function loggedinUser()
             {
                 console.log('in func');
                 var user=request.responseText;
+                alert(user);
               usertxt.innerHTML=` <li id="usertxt" class="dropdown">
                 <div class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><br class="hidden-xs">${user}<span class="caret"></span></div>
                 <ul class="dropdown-menu">
