f69ec8017455fd9a82dabd33ee307dd78db79ea3
varunotelli <varun.kebab@gmail.com>
2016-11-18 13:03:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -66,7 +66,19 @@ app.listen(8080, function () {
   console.log(`IMAD course app listening on port ${port}!`);
 });
 
-
+var pool = new Pool(config);
+app.get('/test-db',function(req,res){
+//make a select response
+////return a response with results
+pool.query("SELECT * FROM test",function(err,result){
+    if(err){
+        res.status(500).send(err.toString());
+    }else{
+        res.send(JSON.stringify(result));
+    }
+    
+});
+});
 
 
 
@@ -208,7 +220,7 @@ res.send(counter.toString());
 
 
 
-*/
+
 
 
 
