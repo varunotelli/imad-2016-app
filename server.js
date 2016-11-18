cb87aab02f4d06840e1b7de1d539d147cbc87f23
varunotelli <varun.kebab@gmail.com>
2016-11-18 14:51:24 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ var app = express();
 app.use(morgan('combined'));
 app.use(bodyParser.json());
 
-app.get('/ui/index.html', function (req, res) {
+app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
