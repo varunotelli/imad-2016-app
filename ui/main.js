d07ec50007428476e8bb41221bd942745bc6ddb0
varunotelli <varun.kebab@gmail.com>
2016-11-18 19:02:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -28,6 +28,36 @@ request.send(JSON.stringify({username:username,password:password,email:email}));
 
 };
 
+var signin=document.getElementById('signinbtn');
+signup.onclick=function()
+{
+    console.log('in func');
+    var request=new XMLHttpRequest();
+    request.onreadystatechange=function(){
+        if(request.readyState===XMLHttpRequest.DONE)
+        {
+            if(request.status===200)
+            window.location.href='http://varunotelli.imad.hasura-app.io/ui/profile.html';
+            else
+            alert('problem');
+        }
+    };
+
+
+
+var username=document.getElementById('usr').value;
+console.log(username);
+var password=document.getElementById('pwd').value;
+console.log(password);
+var email=document.getElementById('email').value;
+console.log(email);
+request.open('POST','http://varunotelli.imad.hasura-app.io/signup',true);
+request.setRequestHeader('Content-Type','application/json');
+request.send(JSON.stringify({username:username,password:password,email:email}));
+
+
+};
+
 
 
 
@@ -63,3 +93,6 @@ request.send(JSON.stringify({title:title,content:content}));
 
 };
 */
+
+
+
