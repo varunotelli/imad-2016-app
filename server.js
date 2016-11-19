430d1a4395dab8adf646512e435bbadd50d3dd9c
varunotelli <varun.kebab@gmail.com>
2016-11-19 16:56:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -112,7 +112,7 @@ function createTemplate(data)
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     
-    <link href="/ui/bootstrap.min.css" rel="stylesheet">
+    <link href="bootstrap.min.css" rel="stylesheet">
 
     
     <link href="https://fonts.googleapis.com/css?family=Prociono" rel="stylesheet">
