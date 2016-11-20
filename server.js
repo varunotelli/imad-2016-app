d64b98c73b64f06c5993839429953c54d6be2902
varunotelli <varun.kebab@gmail.com>
2016-11-20 11:13:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -211,7 +211,16 @@ app.post('/submit-comment/:articleName',function(req,res){
 
 
 
-
+app.get('/get-comments/:articleName', function (req, res) {
+  
+   pool.query('SELECT comments.*, "user".username FROM article, comments, "user" WHERE article.title = $1 AND article.id = comments.article_id AND comment.user_id = "user".id ORDER BY comments.time DESC', [req.params.articleName], function (err, result) {
+      if (err) {
+          res.status(500).send(err.toString());
+      } else {
+          res.send(JSON.stringify(result.rows));
+      }
+   });
+});
 
 
 
