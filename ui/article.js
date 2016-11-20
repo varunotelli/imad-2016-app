8be68024668a23414c919239ffd1ecb99f14d7b8
varunotelli <varun.kebab@gmail.com>
2016-11-20 19:41:16 +0530

    Add files via upload

---
+++ ui/article.js
@@ -0,0 +1,29 @@
+var submit=document.getElementById('submitbtn');
+console.log(submit);
+submit.onclick=function()
+{
+    console.log('in art');
+    var request=new XMLHttpRequest();
+    request.onreadystatechange=function(){
+        if(request.readyState===XMLHttpRequest.DONE)
+        {
+            if(request.status===200)
+            {
+                alert("User created Successfully! Login to continue!");
+               //window.location.href="http://varunotelli.imad.hasura-app.io";
+            
+            }
+            else
+            alert('problem');
+        }
+    };
+
+
+
+var comment=document.getElementById('content');
+request.open('POST','http://varunotelli.imad.hasura-app.io/submit-comment/'+currentArticleTitle,true);
+request.setRequestHeader('Content-Type','application/json');
+request.send(JSON.stringify({comment:comment}));
+
+
+};
