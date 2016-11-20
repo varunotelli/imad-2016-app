bb6dfaa81c830758bea4bc33afeb2b3da071623b
varunotelli <varun.kebab@gmail.com>
2016-11-20 19:25:30 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,35 @@
  var currentArticleTitle = window.location.pathname.split('/')[2];
+
+var submit=document.getElementById('submitbtn');
+console.log(submit);
+submit.onclick=function()
+{
+    console.log('in art');
+    var request=new XMLHttpRequest();
+    request.onreadystatechange=function(){
+        if(request.readyState===XMLHttpRequest.DONE)
+        {
+            if(request.status===200)
+            {
+                alert("User created Successfully! Login to continue!");
+               //window.location.href="http://varunotelli.imad.hasura-app.io";
+            
+            }
+            else
+            alert('problem');
+        }
+    };
+
+
+
+var comment=document.getElementById('content');
+request.open('POST','http://varunotelli.imad.hasura-app.io/submit-comment/'+currentArticleTitle,true);
+request.setRequestHeader('Content-Type','application/json');
+request.send(JSON.stringify({comment:comment}));
+
+
+};
+
  
  function loggedinUser()
 {
@@ -123,35 +154,6 @@ request.send(JSON.stringify({username:username,password:password,email:email}));
 };
 
 
-var submit=document.getElementById('submitbtn');
-console.log(submit);
-submit.onclick=function()
-{
-    console.log('in art');
-    var request=new XMLHttpRequest();
-    request.onreadystatechange=function(){
-        if(request.readyState===XMLHttpRequest.DONE)
-        {
-            if(request.status===200)
-            {
-                alert("User created Successfully! Login to continue!");
-               //window.location.href="http://varunotelli.imad.hasura-app.io";
-            
-            }
-            else
-            alert('problem');
-        }
-    };
-
-
-
-var comment=document.getElementById('content');
-request.open('POST','http://varunotelli.imad.hasura-app.io/submit-comment/'+currentArticleTitle,true);
-request.setRequestHeader('Content-Type','application/json');
-request.send(JSON.stringify({comment:comment}));
-
-
-};
 
 /*
 
