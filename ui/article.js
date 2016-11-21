3513f4689f6cb36939b4528c891c69b4506e1592
varunotelli <varun.kebab@gmail.com>
2016-11-22 00:09:50 +0530

    [imad-console] Updates ui/article.js

--- ui/article.js
+++ ui/article.js
@@ -84,5 +84,44 @@ request.send(null);
  
     
 }
+function loadArticles () {
+        // Check if the user is already logged in
+        console.log('in home');
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function () {
+        if (request.readyState === XMLHttpRequest.DONE) {
+            var articles = document.getElementById('articles');
+            if (request.status === 200) {
+                var content = '<ul>';
+                var articleData = JSON.parse(this.responseText);
+                for (var i=0; i< articleData.length; i++) {
+                    content += `<li>
+                    <a href="/articles/${articleData[i].title}"><div class="articlestuff"><div id="blogtxt">  
+        
+        <center><h2><b>${heading}</b></h2></center>
+        <center><h6>${time.toDateString()}</h6></center>
+        <br>
+        <center>
+    <img src='${image}'>
+    <br>
+    <br>
+    <div>${content}</div>
+    <br>
+    
+    </div>
+    <br><br></div></a></li>`;
+                }
+                content += "</ul>";
+                articles.innerHTML = content;
+            } else {
+                articles.innerHTML('Oops! Could not load all articles!');
+            }
+        }
+    };
+    
+    request.open('GET', '/get-articles', true);
+    request.send(null);
+}
 
+loadArticles();
 loadComments();
