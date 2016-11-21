bd4c28dadaf9969968a9d4ff285e48ed43c7c739
varunotelli <varun.kebab@gmail.com>
2016-11-22 00:04:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -56,6 +56,8 @@ app.get('/ui/profile.html', function (req, res) { res.sendFile(path.join(__dirna
 
 app.get('/ui/ajax-loader.gif', function (req, res) { res.sendFile(path.join(__dirname, 'ui', 'ajax-loader.gif')); });
 
+app.get('/ui/home.html', function (req, res) { res.sendFile(path.join(__dirname, 'ui', 'home.html')); });
+
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
