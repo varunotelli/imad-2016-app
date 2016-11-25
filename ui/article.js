ac6bd14090177ea1e0fb9df2d080a3b94e161fc3
varunotelli <varun.kebab@gmail.com>
2016-11-25 10:04:27 +0530

    [imad-console] Updates ui/article.js

--- ui/article.js
+++ ui/article.js
@@ -33,7 +33,7 @@ function loadCommentForm () {
         };
         
         // Make the request
-        var comment = document.getElementById('comment_text').value;
+        var comment = document.getElementById('content').value;
         request.open('POST', '/submit-comment/' + currentArticleTitle, true);
         request.setRequestHeader('Content-Type', 'application/json');
         request.send(JSON.stringify({comment: comment}));  
