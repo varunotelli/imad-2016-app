4d19bdedae228fbaca3a79cd1d34ad41efccd921
varunotelli <varun.kebab@gmail.com>
2016-11-25 19:26:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -122,11 +122,16 @@ function createTemplate(data)
         <div class="container-fluid">
     
             <div class="navbar-header">
-    
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#collapsemenu" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
             <a href="/ui/home.html"><div id="blogotron" class="navbar-brand">BLOGOTRON</div></a>
             
             </div>
-        
+        <div class="collapse navbar-collapse" id="collapsemenu">
             <ul id="nav-list" class="nav navbar-nav navbar-right">
                 
                
@@ -134,6 +139,7 @@ function createTemplate(data)
                 
             </ul>
        
+    </div>
     </div>
 </nav>
     <div class="container-fluid">
