47e88ce36bf5a54af7662524339a85d07a68432c
varunotelli <varun.kebab@gmail.com>
2016-11-25 13:20:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/favicon.ico', function (req, res) {
+app.get('/ui/favicon.ico', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'favicon.ico'));
 });
 
