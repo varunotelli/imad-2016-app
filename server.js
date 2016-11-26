431fafe3e67e2ca0d8e1c001765ef2580a388b08
varunotelli <varun.kebab@gmail.com>
2016-11-26 23:39:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -121,7 +121,7 @@ function createTemplate(data)
     
     <link href="https://fonts.googleapis.com/css?family=Oswald:700" rel="stylesheet">
     </head>
-<body>
+<body  onload="return loadComments();">
 <nav  class="navbar navbar-default">
         <div class="container-fluid">
     
